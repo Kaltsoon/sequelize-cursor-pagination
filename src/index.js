bcbddaf5aa8c9c4837ece56a3bb1bb415c696085
Benedict Lewis <0x62@users.noreply.github.com>
2018-01-06 16:09:32 +0000

    Add attributes option to allow filtering returned fields

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function getPaginationQuery(cursor, cursorOrderOperator, paginationField, primar
 
 function withPagination({ methodName = 'paginate', primaryKeyField = 'id' } = {}) {
   return model => {
-    const paginate = ({ where = {}, include = [], limit, before, after, desc = false, paginationField = primaryKeyField }) => {
+    const paginate = ({ where = {}, include = [], limit, before, after, desc = false, paginationField = primaryKeyField, attributes = {} }) => {
       const decodedBefore = !!before ? decodeCursor(before) : null;
       const decodedAfter = !!after ? decodeCursor(after) : null;
       const cursorOrderIsDesc = before ? !desc : desc;
@@ -58,6 +58,7 @@ function withPagination({ methodName = 'paginate', primaryKeyField = 'id' } = {}
       return model.findAll({
         where: whereQuery,
         include,
+        attributes,
         limit: limit + 1,
         order: [
           cursorOrderIsDesc ? [paginationField, 'DESC'] : paginationField,
